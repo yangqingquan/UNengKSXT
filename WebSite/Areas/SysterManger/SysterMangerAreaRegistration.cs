48d521f6a909907f77f99ba212cb7b73941b5992
yqq123 <yqq123@f956ef47-3fc9-46b1-b529-9ce39a6973c7>
2017-06-25 14:57:32 +0000

    基础框架搭建

---
+++ WebSite/Areas/SysterManger/SysterMangerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebSite.Areas.SysterManger
+{
+    public class SysterMangerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SysterManger";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SysterManger_default",
+                "SysterManger/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
